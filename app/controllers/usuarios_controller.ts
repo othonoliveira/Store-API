9325fb32056c1b06de7a711c88437513a94eb8d0
Othon Oliveira <othonoliveira08@gmail.com>
2024-07-21 12:20:41 -0300

    Feat: usuarios controller

--- app/controllers/usuarios_controller.ts
+++ app/controllers/usuarios_controller.ts
@@ -2,18 +2,28 @@ import Usuario from '#models/usuario'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UsuariosController {
-  async login({ request }: HttpContext) {
+  async login({ request, response }: HttpContext) {
     try {
-      const data = request.only(['email', 'senha'])
-      const validEmail = await Usuario.findByOrFail(data)
-      await Usuario.findByOrFail(data)
+      const { email, senha } = request.only(['email', 'senha'])
+      const validEmail = await Usuario.findByOrFail({ email })
+      await Usuario.findByOrFail({ senha })
       const token = await Usuario.accessTokens.create(validEmail)
       return {
         type: 'bearer',
         token: token.value!.release(),
       }
     } catch {
-      return { message: 'Credenciais Invalidas' }
+      return response.status(500).json({ message: 'Credenciais Invalidas' })
+    }
+  }
+
+  async cadastro({ request, response }: HttpContext) {
+    try {
+      const data = request.only(['email', 'senha'])
+      const user = await Usuario.create(data)
+      return user
+    } catch {
+      return response.status(500).json({ message: 'Usuario já cadastrado' })
     }
   }
 }
